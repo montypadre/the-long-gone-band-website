5e6c6134d9f746adf09847009aaa920cc22add46
root <root@ndmaddhouse.com>
2021-07-23 12:29:29 -0500

    Initial commit

---
+++ form-to-email.php
@@ -0,0 +1,47 @@
+<?php
+$errors = '';
+$myemail = 'montypadre77@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "Potential Gig Alert: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+$success = mail($to,$email_subject,$email_body,$headers);
+
+}
+if ($success)
+{
+    echo "<p style='text-align:center;'>Thanks " . $name . "!</p><p style='text-align:center;'>We will contact you very soon!.</p>";
+}
+?>
